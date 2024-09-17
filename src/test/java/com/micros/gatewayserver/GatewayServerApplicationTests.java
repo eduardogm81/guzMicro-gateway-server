49a9d54508093a3fbe069ecca20b1f0107505972
egomez <egomez@nundusoft.com>
2024-09-17 11:53:04 +0200

    Commit inicial

---
+++ src/test/java/com/micros/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micros.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
